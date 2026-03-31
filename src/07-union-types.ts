ca0d27e587f2ec465f89868d6b3fe8f6ed08ff61
Tomas Hernandez <kevinhdez592@gmail.com>
2026-03-31 10:01:30 -0600

    intersection types

--- src/07-union-types.ts
+++ src/07-union-types.ts
@@ -2,17 +2,24 @@
 // Optional Properties
 
 // Template Literal Types
-type HeroId = `${string}-${string}-${string}-${string}-${string}`;
-type HeroPowerScale = "local" | "planetary" | "galactic" | "universal" | "multiversal" | number;
+type HeroId = `${string}-${string}-${string}-${string}-${string}`
+type HeroPowerScale = "local" | "planetary" | "galactic" | "universal" | "multiversal"
 
-type Hero = {
-    // readonly: Indica que la propiedad no se puede modificar
-    readonly id?: HeroId;
-    name: string;
-    age: number;
-    isActive?: boolean;
-    powerScale?: HeroPowerScale;
+type HeroBasicInfo = {
+    name: string
+    age: number
 }
+
+type HeroProperties = {
+    // readonly: Indica que la propiedad no se puede modificar
+    readonly id?: HeroId
+    isActive?: boolean
+    powerScale?: HeroPowerScale
+}
+
+// Intersection Types: Hero será de tipo HeroProperties y HeroBasicInfo a la vez,
+// es decir, tendrá todas las propiedades de ambos types
+type Hero = HeroProperties & HeroBasicInfo
 
 const ironMan: Hero = {
     name: "Iron Man",
@@ -20,22 +27,22 @@ const ironMan: Hero = {
     isActive: false,
 }
 
-console.log(ironMan);
+ironMan.powerScale = "local"
+console.log(ironMan)
 
-
-const createHero = (name: string, age: number): Hero => {
+const createHero = (basicHero: HeroBasicInfo): Hero => {
+    const { name, age } = basicHero
     return {
         id: crypto.randomUUID(),
         name,
         age,
         isActive: true,
-    };
+    }
 }
 
-const thor = createHero("Thor", 1500);
-console.log(thor.id?.toString()); // Optional Chaining
-thor.powerScale = "galactic";
-console.log(thor);
+const thor = createHero({ name: "Thor", age: 1500 })
+console.log(thor.id?.toString()) // Optional Chaining
+thor.powerScale = "galactic"
+console.log(thor)
 
 // thor.id = 23232 // Error: Cannot assign to 'id' because it is a read-only property.
-
